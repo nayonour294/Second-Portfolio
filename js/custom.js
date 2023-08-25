385cb363c224a206962fd7cb1b14d8ffa49c4740
nayonour294 <nayonour294@gmail.com>
2023-08-26 04:35:12 +0600

    Second-Portfolio
    Portfolio Code uploaded

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// Typeing Script
+
+var typed = new Typed (".typing", {
+    strings: ["Project Leader", "Web Designer", "Web Development",],
+    typeSpeed: 100,
+    backDelay: 900,
+    backSpeed: 50,
+    loop: true,
+});
